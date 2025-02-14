ad65fc86279b07f0dd29b73fb155ace5ebfee55b
namphuong <phuongnampham7823@gmail.com>
2025-02-14 13:37:35 +0700

    fix: config server in main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 
-const server = express(); // Create an Express server
+const server = express();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors(); // Enable CORS if needed
+  app.enableCors();
   await app.init();
 }
 
 bootstrap();
 
-export default server; // Important for Vercel
+export default server;
