c5aa9391f42ce7584e855db7bea49f1780bf8efe
namphuong <phuongnampham7823@gmail.com>
2025-02-14 14:05:12 +0700

    fix: port main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors();
-  await app.init();
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 8000);
 }
-
 bootstrap();
-
-export default server;
