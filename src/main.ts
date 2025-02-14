abe8ede9070c9845497f539460917cdcbef22c37
namphuong <phuongnampham7823@gmail.com>
2025-02-14 15:56:41 +0700

    add allow cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: [
+      "http://localhost:3000", // Local frontend
+      "https://library-fe-delta.vercel.app" // Deployed frontend on Vercel
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Allow cookies if needed
+  });
+
   await app.listen(process.env.PORT || 8000,);
 }
 bootstrap();
